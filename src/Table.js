78823c89560072d8aee3508818cbfc2542167bed
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-09-21 04:07:05 -0400

    thead created

--- src/Table.js
+++ src/Table.js
@@ -6,7 +6,43 @@ const Table = ({ users }) => {
     return (
         <div>
             <table className='table'>
-                
+                <thead>
+                    <tr>
+                        <th scope='col'>Title</th>
+                        <th
+                            scope='col'
+                            onClick={() => {
+                                const usersCopy = [...users];
+                                const updateSort = usersCopy.sort((a, b) => {
+                                    const nameA = a.name.first;
+                                    const nameB = b.name.first;
+
+                                    if(nameA < nameB) {
+                                        return -1;
+                                    }
+                                    if(nameA > nameB) {
+                                        return 1;
+                                    }
+                                     
+                                    return 0;
+                                    });
+                                    updateSortedUsers(updateSort);
+                                }}
+                            >
+                                First
+                                </th>
+                                <th scope='col'>Last</th>
+                                <th scope='col'>Gender</th>
+                                <th scope='col'>Email</th>
+                                <th scope='col'>Phone</th>
+                                <th scope='col'>City</th>
+                                <th scope='col'>Country</th>
+                                <th scope='col'>Picture</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
             </table>
         </div>
     )
